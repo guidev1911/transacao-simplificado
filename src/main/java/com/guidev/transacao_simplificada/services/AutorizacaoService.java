1a5c5d4902148b2b29bcc31d098f8180b59f9d08
guidev1911 <guilhermereal1911@gmail.com>
2025-04-04 22:53:16 -0300

    autorizacao service

---
+++ src/main/java/com/guidev/transacao_simplificada/services/AutorizacaoService.java
@@ -0,0 +1,21 @@
+package com.guidev.transacao_simplificada.services;
+
+import com.guidev.transacao_simplificada.infrastructure.clients.AutorizacaoClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class AutorizacaoService {
+
+    private final AutorizacaoClient client;
+
+    public boolean validarTransferencia(){
+        if(Objects.equals(client.validarAutorizacao().data().authorization(), "true")){
+            return true;
+        }
+        return false;
+    }
+}
