00b652535e3008aafd57a6a99da85754a0550c02
guidev1911 <guilhermereal1911@gmail.com>
2025-04-10 21:39:31 -0300

    subir docker

--- src/main/java/com/guidev/transacao_simplificada/controller/TransacaoController.java
+++ src/main/java/com/guidev/transacao_simplificada/controller/TransacaoController.java
@@ -8,16 +8,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/transfer")
 public class TransacaoController {
-        private final TransacaoService transacaoService;
 
-        @PostMapping
-        public ResponseEntity<Void> realizarTransacao(@RequestBody TransacaoDTO transacaoDTO){
-            transacaoService.transferirValores(transacaoDTO);
-            return ResponseEntity.accepted().build();
-        }
+    private final TransacaoService transacaoService;
+
+    @PostMapping
+    public ResponseEntity<Void> realizarTransacao(@RequestBody TransacaoDTO transacaoDTO){
+        transacaoService.transferirValores(transacaoDTO);
+        return ResponseEntity.accepted().build();
+    }
 }
--- src/main/java/com/guidev/transacao_simplificada/services/TransacaoService.java
+++ src/main/java/com/guidev/transacao_simplificada/services/TransacaoService.java
@@ -7,9 +7,9 @@ import com.guidev.transacao_simplificada.infrastructure.entities.Transacoes;
 import com.guidev.transacao_simplificada.infrastructure.entities.Usuario;
 import com.guidev.transacao_simplificada.infrastructure.exceptions.BadRequestException;
 import com.guidev.transacao_simplificada.infrastructure.repository.TransacaoRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpClientErrorException;
 
 import java.math.BigDecimal;
@@ -21,69 +21,72 @@ public class TransacaoService {
     private final UsuarioService usuarioService;
     private final AutorizacaoService autorizacaoService;
     private final CarteiraService carteiraService;
-    private final TransacaoRepository transacaoRepository;
+    private final TransacaoRepository repository;
     private final NotificacaoService notificacaoService;
 
     @Transactional
-    public void transferirValores(TransacaoDTO trasacaoDTO){
-        Usuario pagador = usuarioService.buscarUsuario(trasacaoDTO.payer());
-        Usuario recebedor = usuarioService.buscarUsuario(trasacaoDTO.payee());
+    public void transferirValores(TransacaoDTO transacaoDTO) {
+        Usuario pagador = usuarioService.buscarUsuario(transacaoDTO.payer());
+        Usuario recebedor = usuarioService.buscarUsuario(transacaoDTO.payee());
 
         validaPagadorLojista(pagador);
-        validarSaldoUsuario(pagador, trasacaoDTO.value());
+        validarSaldoUsuario(pagador, transacaoDTO.value());
         validarTransferencia();
 
-        pagador.getCarteira().setSaldo(pagador.getCarteira().getSaldo().subtract(trasacaoDTO.value()));
+        pagador.getCarteira().setSaldo(pagador.getCarteira().getSaldo().subtract(transacaoDTO.value()));
         atualizarSaldoCarteira(pagador.getCarteira());
-        recebedor.getCarteira().setSaldo(pagador.getCarteira().getSaldo().add(trasacaoDTO.value()));
+        recebedor.getCarteira().setSaldo(pagador.getCarteira().getSaldo().add(transacaoDTO.value()));
         atualizarSaldoCarteira(recebedor.getCarteira());
 
         Transacoes transacoes = Transacoes.builder()
-                .valor(trasacaoDTO.value())
+                .valor(transacaoDTO.value())
                 .pagador(pagador)
                 .recebedor(recebedor)
                 .build();
 
-        transacaoRepository.save(transacoes);
+        repository.save(transacoes);
         enviarNotificacao();
     }
 
-    private void validaPagadorLojista(Usuario usuario){
-        try{
-            if(usuario.getTipoUsuario().equals(TipoUsuario.LOJISTA)){
-                throw new IllegalArgumentException("Transação não autorizada para esse tipo de usuário");
+    private void validaPagadorLojista(Usuario usuario) {
+        try {
+            if (usuario.getTipoUsuario().equals(TipoUsuario.LOJISTA)) {
+                throw new IllegalArgumentException("Transação não autorizada para esse tipo de usuario");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new IllegalArgumentException(e.getMessage());
         }
     }
-    private void validarSaldoUsuario(Usuario usuario, BigDecimal valor){
-        try{
-            if (usuario.getCarteira().getSaldo().compareTo(valor) < 0){
+
+    private void validarSaldoUsuario(Usuario usuario, BigDecimal valor) {
+        try {
+            if (usuario.getCarteira().getSaldo().compareTo(valor) < 0) {
                 throw new IllegalArgumentException("Transação não autorizada, saldo insuficiente");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new IllegalArgumentException(e.getMessage());
         }
     }
-    private void validarTransferencia(){
-        try{
-            if (!autorizacaoService.validarTransferencia()){
-                throw new IllegalArgumentException("Transação não autorizada pela API");
+
+    private void validarTransferencia() {
+        try {
+            if (!autorizacaoService.validarTransferencia()) {
+                throw new IllegalArgumentException("Transação não autorizada pela api");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new IllegalArgumentException(e.getMessage());
         }
     }
-    private void atualizarSaldoCarteira(Carteira carteira){
+
+    private void atualizarSaldoCarteira(Carteira carteira) {
         carteiraService.salvar(carteira);
     }
 
-    private void enviarNotificacao(){
-        try{
+    private void enviarNotificacao() {
+        try {
             notificacaoService.enviarNotificacao();
-        }catch (HttpClientErrorException e){
-            throw new BadRequestException("Erro ao enviar notificação");
+        } catch (HttpClientErrorException e) {
+            throw new BadRequestException("Erro ao enviar notificacao");
         }
     }
 
