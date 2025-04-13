36fec42184412db9adeccedca750ec7190167239
guidev1911 <guilhermereal1911@gmail.com>
2025-04-12 21:53:09 -0300

    excessão userNotFound

--- src/main/java/com/guidev/transacao_simplificada/controller/GlobalExceptionHandler.java
+++ src/main/java/com/guidev/transacao_simplificada/controller/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.guidev.transacao_simplificada.controller;
 
 import com.guidev.transacao_simplificada.infrastructure.exceptions.BadRequestException;
+import com.guidev.transacao_simplificada.infrastructure.exceptions.UserNotFound;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,5 +18,10 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handlerBadRequestException(BadRequestException e){
         return new ResponseEntity<>("Erro: "+ e.getMessage(), HttpStatus.BAD_REQUEST);
     }
+    @ExceptionHandler(UserNotFound.class)
+    public ResponseEntity<String> handlerUserNotFoundException(UserNotFound e){
+        return new ResponseEntity<>("Erro: "+ e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
 
 }
